109b1499146535332339f5612148bae34ffb6f74
Randi Schreiner <mimirandi21@gmail.com>
2021-01-06 15:34:10 -0600

    trial of new webpack.config

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,3 +3,4 @@
 //= require turbolinks
 //= require bootstrap-sprockets
 //= require_tree .
+//= require webpack.bundle.js
--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,17 @@
-var CopyWebpackPlugin = require('copy-webpack-plugin');
-
 module.exports = {
-  entry: './src/js/main.js',
-
-  output: {
-    path: './dist',
-    filename: 'bundle.js'
-  },
-
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel',
-        query: {
-            presets: ['es2015']
-        }
-      },
-      {
-        test: /\.less$/,
-        loader: "style!css!less"
-      },
-      {
-        test: /\.(jpg|png|gif)$/,
-        include: /img/,
-        loader: 'url'
-      },
-    ]
-  },
-
-  plugins: [
-    new CopyWebpackPlugin([
-      { from: './src/index.html' }
-    ]),
-    new CopyWebpackPlugin([
-      { from: './src/vendors/phaser.min.js' }
-    ]),
-    new CopyWebpackPlugin([
-      { from: './src/assets', to: 'assets' }
-    ])
-  ],
-
-  resolve: {
-    extensions: ['', '.js', '.jsx']
-  },
-
-  devServer: {
-    inline: true,
-    contentBase: './dist',
-    port: 5000
-  }
+	context: __dirname + "/javascripts",
+	entry: "./webpack_application.js",
+	output: {
+		filename: "webpack.bundle.js",
+		path: __dirname + "/javascripts",
+	},
+	module: {
+		rules: [
+			{ test: /\.jsx?$/, exclude: /node_modules/, loader: "babel-loader" },
+		],
+	},
+	resolve: {
+		extensions: ["", ".js", ".jsx"],
+		modulesDirectories: ["node_modules", "javascripts"],
+	},
 };
