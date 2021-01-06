8c0a1291c8834ab683b1492442c4ca99be2723f5
Randi Schreiner <mimirandi21@gmail.com>
2021-01-06 08:49:42 -0600

    still working through webpack issues

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -1,5 +1,15 @@
 // Load all the channels within this directory and all subdirectories.
 // Channel files must be named *_channel.js.
 
-const channels = require.context('.', true, /_channel\.js$/)
-channels.keys().forEach(channels)
+const channels = require.context(".", true, /_channel\.js$/);
+channels.keys().forEach(channels);
+
+const express = require("express");
+const canvas = require("canvas");
+const PORT = process.env.PORT || 5000;
+
+express()
+	.get("/", (req, res) =>
+		res.send({ version: canvas.version, cairoVersion: canvas.cairoVersion })
+	)
+	.listen(PORT, () => console.log(`Listening on ${PORT}`));
